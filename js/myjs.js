9fd5037db6ad600493945ca0a4ebb7c0305743cf
RunCmd <neskoskil@gmail.com>
2016-10-04 21:02:28 +0200

    desktop version done, everything included for all files

--- js/myjs.js
+++ js/myjs.js
@@ -1,5 +1,11 @@
 // Author:Daniel Neshkoski
 
 $(document).ready(function(){
+	$(".dropdown-large").on("click",function(e){
+		$('.drop-caret').toggle();
+	});
+});
 
+$(document).on("click", function () {
+    $(".drop-caret").hide();
 });
