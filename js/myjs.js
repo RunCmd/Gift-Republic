cb252617ce5903b594a6a14916b2711cd519b8ab
RunCmd <neskoskil@gmail.com>
2016-10-09 17:42:10 +0200

    product-list item pages added new style

--- js/myjs.js
+++ js/myjs.js
@@ -7,6 +7,17 @@ $(document).ready(function(){
 	$(".dropdown-menu-large").hover(function(e){
 		$('.drop-caret').show();
 	});
+	// On scroll down Main Menu Mobile
+	if($(window).width() < 768){
+	 $(window).scroll(function() {
+        if ($(document).scrollTop() > 100) {
+            $('.main-navigation .navbar-header').css({'background':"white", "width": "100%"});
+        }
+        else {
+            $('.main-navigation .navbar-header').css("background","transparent");
+        }
+    });
+	}
 });
 
 $(document).hover(function () {
