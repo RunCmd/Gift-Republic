704ad20436d63ab716d7a9b0074b10b4385058d8
RunCmd <neskoskil@gmail.com>
2016-10-10 21:35:24 +0200

    New Logo, new hover efects , Mobile menu fixed top

--- js/myjs.js
+++ js/myjs.js
@@ -10,14 +10,19 @@ $(document).ready(function(){
 	// On scroll down Main Menu Mobile
 	if($(window).width() < 768){
 	 $(window).scroll(function() {
-        if ($(document).scrollTop() > 100) {
-            $('.main-navigation .navbar-header').css({'background':"white", "width": "100%"});
-        }
-        else {
-            $('.main-navigation .navbar-header').css("background","transparent");
-        }
+        // if ($(document).scrollTop() > 100) {
+        //     $('.main-navigation .navbar-header').css({'background':"white", "width": "100%"});
+        // }
+        // else {
+        //     $('.main-navigation .navbar-header').css({"background":"white", "width": "100%"});
+        // }
     });
 	}
+	//Product Image replace
+	$('.show-product img').hover(function() {
+    	var url = $(this).attr('src');
+    	$('#main-image').attr('src', url).css("width", "355px");
+	});
 });
 
 $(document).hover(function () {
