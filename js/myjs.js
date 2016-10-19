a4fc4b47bacb68b6803c25679ae03eb05a6dbe45
RunCmd <neskoskil@gmail.com>
2016-10-19 21:57:47 +0200

    Header Fixed, added button to top link container fixed

--- js/myjs.js
+++ js/myjs.js
@@ -23,10 +23,26 @@ $(document).ready(function(){
     	var url = $(this).attr('src');
     	$('#main-image').attr('src', url).css("width", "355px");
 	});
+//To TOP LINK default 
+        $('#top-page').click(function(){
+            $("html, body").animate({ scrollTop: 0 }, 1100);
+            return false;
+        });
 });
 
 $(document).hover(function () {
     $(".drop-caret").hide();
 });
+	//Sticky Header
+	$(window).scroll(function() {
 
+	if ($(this).scrollTop() > 230 && $(this).width() > 992){  
+	    $('.main-navigation .navbar').addClass("sticky");
+	    $('.logo-main>img').addClass("sticky-logo");
+	  }
+	  else{
+	    $('.main-navigation .navbar').removeClass("sticky");
+	    $('.logo-main>img').removeClass("sticky-logo");
+	  }
+	});
 
